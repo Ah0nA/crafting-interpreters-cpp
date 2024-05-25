e7a47040811b863754ef9000259d7111d417c4fc
Amanda AFA <marssarek@gmail.com>
2024-05-26 00:36:47 +0300

    Add: full  whole Scanner class

--- lox scanner.cpp
+++ lox scanner.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <variant>
+#include <unordered_map>
 #include <Tokentype.h>
 
 class Token
@@ -205,6 +206,56 @@ private:
         case '*':
             addToken(TokenType::STAR);
             break;
+        case '!':
+            addToken(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG);
+            break;
+        case '=':
+            addToken(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL);
+            break;
+        case '<':
+            addToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS);
+            break;
+        case '>':
+            addToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
+            break;
+        case '/':
+            if (match('/'))
+            {
+                while (peek() != '\n' && !isAtEnd())
+                    advance();
+            }
+            else
+            {
+                addToken(TokenType::SLASH);
+            }
+        case ' ':
+        case '\r':
+        case '\t':
+            break;
+        case '\n':
+            line++;
+            break;
+        case '"':
+            string();
+            break;
+
+        default:
+            if (isdigit(c))
+            {
+                number();
+            }
+            else if (isalpha(c))
+            {
+                identifier();
+            }
+            else
+            {
+                std::string errorMessage = "Unexpected character: ";
+                errorMessage += c;
+                Lox::error(line, errorMessage);
+            }
+
+            break;
         }
     }
 
@@ -212,6 +263,91 @@ private:
     {
         current++;
         return source[current - 1];
+    }
+
+    bool Scanner::match(char expected)
+    {
+        if (isAtEnd())
+            return false;
+        if (source[current != expected])
+            return false;
+        current++;
+        return true;
+    }
+
+    char peek()
+    {
+        if (isAtEnd())
+            return '\0';
+        return source[current];
+    }
+
+    void Scanner::string()
+    {
+        while (peek() != '"' && isAtEnd())
+        {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+        if (isAtEnd())
+        {
+            std::string errorNote = "Unterminated string";
+            Lox::error(line, errorNote);
+            return;
+        }
+        advance();
+
+        std::string value = source.substr(start + 1, current - start - 2);
+        addToken(TokenType::STRING, value);
+    }
+
+    bool isDigit(char c)
+    {
+        return c >= 0 && c <= 9;
+    }
+
+    void Scanner::number()
+    {
+        while (isDigit(peek()))
+            advance();
+
+        if (peek() == '.' && isDigit(peekNext()))
+        {
+            advance();
+
+            while (isDigit(peek()))
+                advance();
+        }
+        std::string numberString = source.substr(start, current - start);
+        addToken(TokenType::NUMBER, std::stod(numberString));
+    }
+
+    bool isAlpha(char c)
+    {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_');
+    }
+
+    bool isAlphaNumeric(char c)
+    {
+        return isDigit || isAlpha;
+    }
+
+    void Scanner::identifier()
+    {
+        while (isAlphaNumeric(peek()))
+            advance();
+        std::string text = source.substr(start, current - start);
+        auto it = keywords.find(text);
+        TokenType type = (it != keywords.end()) ? it -> second : TokenType::IDENTIFIER;
+        addToken(type);
+    }
+
+    char peekNext()
+    {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source[current + 1];
     }
 
     void Scanner::addToken(TokenType type)
@@ -224,6 +360,23 @@ private:
         std::string text = source.substr(start, current - start);
         tokens.emplace_back(type, text, literal, line);
     }
+    const std::unordered_map<std::string, TokenType> Scanner::keywords = {
+        {"and", TokenType::AND},
+        {"class", TokenType::CLASS},
+        {"else", TokenType::ELSE},
+        {"false", TokenType::FALSE},
+        {"for", TokenType::FOR},
+        {"fun", TokenType::FUN},
+        {"if", TokenType::IF},
+        {"nil", TokenType::NIL},
+        {"or", TokenType::OR},
+        {"print", TokenType::PRINT},
+        {"return", TokenType::RETURN},
+        {"super", TokenType::SUPER},
+        {"this", TokenType::THIS},
+        {"true", TokenType::TRUE},
+        {"var", TokenType::VAR},
+        {"while", TokenType::WHILE}};
 };
 
 class Lox
