ba84f6e2140c03db80354448944201f17727e3fe
Amanda AFA <marssarek@gmail.com>
2024-06-19 13:48:38 +0300

    add: adds basic utility methods

--- Parser.cpp
+++ Parser.cpp
@@ -1,2 +1,60 @@
-#include <parser.h>
+#include "Parser.h"
+
+// tokens -> AST
+
+Parser::Parser(const std::vector<Token> &tokens) : tokens(tokens) {}
+
+Token Parser::advance()
+{
+    if (!isAtEnd())
+        current++;
+    return previous();
+}
+
+bool Parser::isAtEnd()
+{
+    return peek().type == TokenType::END_OF_FILE;
+}
+
+Token Parser::peek()
+{
+    return tokens[current];
+}
+
+Token Parser::previous()
+{
+    return tokens[current - 1];
+}
+
+bool Parser::check(TokenType type){
+    if(isAtEnd) return false;
+    return peek().type == type;
+}
+
+bool Parser::match(const std::vector<TokenType> &types){
+    for(TokenType type: types){
+        if(check(type)){
+            advance();
+            return true;
+        }
+    }
+    return false;
+}
+
+std::shared_ptr<Expr> Parser::expression()
+{
+    return equality();
+}
+
+// equality → comparison ( ( "!=" | "==" ) comparison )* ;
+
+std::shared_ptr<Expr> Parser::equality()
+{
+    auto expr = Parser::comparision();
+
+    while (match({TokenType::BANG_EQUAL, TokenType::EQUAL_EQUAL}))
+    {
+
+    }
+}
 
--- parser.h
+++ parser.h
@@ -12,7 +12,17 @@ private:
     std::vector<Token> tokens;
     int current = 0;
 
-    public:
-    Parser(const std::vector<Token> tokens) : tokens(tokens){}
-    
+public:
+    Parser(const std::vector<Token> &tokens);
+
+private:
+    std::shared_ptr<Expr> expression();
+    std::shared_ptr<Expr> equality();
+    bool match(const std::vector<TokenType> &types);
+    bool check(TokenType type);
+    Token advance();
+    bool isAtEnd();
+    Token peek();
+    Token previous();
+    std::shared_ptr<Expr> comparision();
 };
